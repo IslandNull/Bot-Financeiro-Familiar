e0ada7141fe697a476cee034dc0024f724417cce
Gustavo Ilha <guga_ilha99@hotmail.com>
2026-05-25 14:18:00 -0300

    fix: pin Val Town proxy import

--- val-town/main.ts
+++ val-town/main.ts
@@ -1,1 +1,1 @@
-export { default } from "https://raw.githubusercontent.com/IslandNull/Bot-Financeiro-Familiar/codex/telegram-ux-revamp/val-town/telegram-proxy.ts?v=telegram-silent-callback-20260525";
+export { default } from "https://raw.githubusercontent.com/IslandNull/Bot-Financeiro-Familiar/a1888ce04bfe235ef9bc74b0a7eb0ef5f31f195b/val-town/telegram-proxy.ts?v=telegram-silent-callback-20260525";
